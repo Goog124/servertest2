8fd0c08af22a93c5ec35494e3ffbdc186e2c75c1
ExampleTestUser <teacher@techer.com>
2026-02-12 19:27:31 +0300

    dsad2

--- maper.py
+++ maper.py
@@ -30,7 +30,7 @@ class GameView(arcade.Window):
 
         api_server = "https://static-maps.yandex.ru/v1"
 
-        geocode = "Великий Новгород"
+        geocode = "Австралия"
         apikey_static = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
         apikey_geo = "8013b162-6b42-4997-9691-77b7074026e0"
 
---
+++ search.py
@@ -0,0 +1,55 @@
+import sys
+from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
+
+import requests
+from PIL import Image
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "73ea1519-8fff-4060-b918-768a12d35d45",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+
+if not response:
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+# Координаты центра топонима:
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_size = toponym["boundedBy"]["Envelope"]
+x1, y1 = toponym_size["lowerCorner"].split()
+x2, y2 = toponym_size["upperCorner"].split()
+toponym_size = (float(x2) - float(x1), float(y2) - float(y1))
+
+print(toponym_size)
+# Долгота и широта:
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+apikey = "5815d7d2-6bbe-424d-a32d-028b8c596fa2"
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([str(toponym_size[0]), str(toponym_size[1])]),
+    "apikey": apikey,
+
+}
+
+map_api_server = "https://static-maps.yandex.ru/v1"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+im = BytesIO(response.content)
+opened_image = Image.open(im)
+opened_image.show()  # Создадим картинку и тут же ее покажем встроенным просмотрщиком операционной системы
